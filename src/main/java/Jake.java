cfcfc044f039e99c926c79e0ef87b20580f94e59
Lyam-T <e1496889@u.nus.edu>
2025-01-21 22:55:50 +0800

    Implement an initial version of Jake

---
+++ src/main/java/Jake.java
@@ -0,0 +1,16 @@
+public class Jake {
+    public static void main(String[] args) {
+        String name = "Jake";
+        String separator = "-----------------------------------------------------------------";
+
+        System.out.println(separator);
+        System.out.println("Hello! My name is "
+                            + name
+                            + "... Why I am here again?");
+        System.out.println("Anyway, what can I do for you this time?");
+        System.out.println(separator);
+
+        System.out.println("Bye! Don't find me again, do it yourself!");
+        System.out.println(separator);
+    }
+}
