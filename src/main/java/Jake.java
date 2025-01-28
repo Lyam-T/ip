799763029cfa08f0b317c5500f9a2ef9e3d49b8e
Lyam-T <e1496889@u.nus.edu>
2025-01-28 16:07:03 +0800

    Add echo function to echo the user input

--- src/main/java/Jake.java
+++ src/main/java/Jake.java
@@ -1,16 +1,32 @@
+import java.util.Scanner;
+
 public class Jake {
+    public static void echo(String input) {
+        System.out.println("  " + input);
+    }
+
     public static void main(String[] args) {
         String name = "Jake";
-        String separator = "-----------------------------------------------------------------";
+        String lineSeparator = "-----------------------------------------------------------------";
+        Scanner in = new Scanner(System.in);
+        String input;
 
-        System.out.println(separator);
+        System.out.println(lineSeparator);
         System.out.println("Hello! My name is "
                             + name
                             + "... Why I am here again?");
         System.out.println("Anyway, what can I do for you this time?");
-        System.out.println(separator);
+        System.out.println(lineSeparator);
+
+        input = in.nextLine();
+        while (!"bye".equalsIgnoreCase(input)) {
+            System.out.println("  " + lineSeparator.substring(2));
+            echo(input);
+            System.out.println("  " + lineSeparator.substring(2));
+            input = in.nextLine();
+        }
 
         System.out.println("Bye! Don't find me again, do it yourself!");
-        System.out.println(separator);
+        System.out.println(lineSeparator);
     }
 }
