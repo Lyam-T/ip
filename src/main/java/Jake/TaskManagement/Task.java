e35d3f1cee049a1e9a31330598385df87bd575d3
Lyam-T <e1496889@u.nus.edu>
2025-02-15 20:17:15 +0800

    Fix unneeded mark/unmark with message prompt

--- src/main/java/Jake/TaskManagement/Task.java
+++ src/main/java/Jake/TaskManagement/Task.java
@@ -9,10 +9,18 @@ public class Task {
     }
 
     public void markDone() {
+        if (isDone) {
+            System.out.println("Task is already done! Don't need to mark it done again.");
+        }
+
         isDone = true;
     }
 
     public void markUndone() {
+        if (!isDone) {
+            System.out.println("Task is already undone! Don't need to mark it undone again.");
+        }
+        
         isDone = false;
     }
 
